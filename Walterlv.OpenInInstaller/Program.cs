30164e6bda7c7052dda6db12a90709dbeefc85c7
walterlv <xblero@gmail.com>
2019-04-21 15:11:33 +0800

    unzip

--- Walterlv.OpenInInstaller/Program.cs
+++ Walterlv.OpenInInstaller/Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
 
 namespace Walterlv.OpenInInstaller
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args) => args switch
         {
-            Console.WriteLine("Hello World!");
+            { Length: 1 } => HandleSingleFile(args[0]),
+            _ => 0,
+        };
+
+        private static int HandleSingleFile(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+            if (".exe".Equals(extension))
+            {
+                var sourceDirectory = Path.GetDirectoryName(fileName);
+                var targetDirectory = Path.Combine(sourceDirectory, Path.GetFileNameWithoutExtension(fileName));
+                if (!Directory.Exists(targetDirectory))
+                {
+                    UnzipInstaller(fileName, targetDirectory);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("目标目录已经存在。");
+                    Console.ResetColor();
+                }
+                return 0;
+            }
+
+            return 0;
+        }
+
+        private static void UnzipInstaller(string fileName, string outputDirectory)
+        {
+            ZipArchive zipArchive = ZipFile.Open(fileName, ZipArchiveMode.Read);
+            var files = zipArchive.Entries.Select(e => e.FullName).ToList();
+            zipArchive.ExtractToDirectory(outputDirectory);
         }
     }
 }
